d9599302fe3b1535e14793ef685877e1c0cc13e2
Celariox <johanchabassiere@gmail.com>
2020-04-07 11:49:15 +0200

    Five exercises

---
+++ exo_02.rb
@@ -0,0 +1,3 @@
+puts "Bonjour monde !"
+puts "Et avec une voix sexy, ça donne : Bonjour, monde !"
+#puts permet d'ajouter une ligne à chaque fin d'argument si cela n'est déjà fait tandis que print n'en ajoute pas.
---
+++ exo_04.rb
@@ -0,0 +1,2 @@
+#Cela va afficher une erreur car la chaîne de caractères n'est pas fermée. 
+puts "Salut, ça farte ?
