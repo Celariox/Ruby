36856005e25a03e02852701e40a0135e1bfbe6e1
Celariox <johanchabassiere@gmail.com>
2020-04-07 12:44:30 +0200

    add exercise 5 and 6

---
+++ exo_05.rb
@@ -0,0 +1,43 @@
+#Le : #{} permet de mettre du code au sein d'une chaîne. Il s'agit de l'opérateur d'interpolation numérique. 
+
+#Va afficher la chaîne de caractères entre parenthèses. -> On va compter le nombre d'heures de travail à THP
+puts "On va compter le nombre d'heures de travail à THP"
+
+#Va afficher la chaîne de caractères Travail : et le résultat du calcul soit 550. Donc -> Travail : 550
+puts "Travail : #{10 * 5 * 11}"
+
+#Va afficher : En minutes ça fait : 33000
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}"
+
+#Va afficher : Et en secondes ?
+puts "Et en secondes ?"
+
+#Va afficher : 1980000
+puts 10 * 5 * 11 * 60 * 60
+
+#Va afficher la chaîne de caractères : Est-ce vrai que 3 + 2 < 5 -7 ?
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"
+
+#Va afficher false car 3 + 2 < 5 - 7 est incorrect. 
+puts 3 + 2 < 5 - 7
+
+#Va afficher : Ca fait combien 3 + 2 ? 5
+puts "Ça fait combien 3 + 2 ? #{3 + 2}"
+
+#Va afficher : Ca fait combien 5 - 7 ? -2
+puts "Ça fait combien 5 - 7 ? #{5 - 7}"
+
+#Va afficher : Ok, c'est faux alors !
+puts "Ok, c'est faux alors !"
+
+#Va afficher : C'est drôle ça, faisons-en plus :
+puts "C'est drôle ça, faisons-en plus :"
+
+#Va afficher : Est-ce que 5 est plus grand que -2 ? True
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}"
+
+#Va afficher : Est-ce que 5 est supérieur ou égal à -2 ? True
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}"
+
+#Va afficher : Est-ce que 5 est inférieur ou égal à -2 ? False
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}"
---
+++ exo_06.rb
@@ -0,0 +1,9 @@
+number_of_hours_worked_per_day = 10
+number_of_days_worked_per_week = 5
+number_of_weeks_in_THP = 11
+
+puts "Travail : #{number_of_hours_worked_per_day * number_of_days_worked_per_week * number_of_weeks_in_THP}"
+#Va afficher le nombre d'heures travaillées au total -> 550
+
+puts "Et en minutes ça fait : #{number_of_minutes_in_an_hour * number_of_hours_worked_per_day * number_of_days_worked_per_week * number_of_weeks_in_THP}"
+#Va afficher une erreur car number_of_minutes_in_an_hour n'a pas été assigné. Il faudrai assigner à la variable précédente la valeur 60. 
